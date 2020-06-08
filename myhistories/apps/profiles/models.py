4a6d3180156e1ecc563bb11aed59321987713e68
npujol <naivy.luna@gmail.com>
2020-06-08 21:05:26 +0200

    Update bio and image with null=True

--- myhistories/apps/profiles/models.py
+++ myhistories/apps/profiles/models.py
@@ -5,8 +5,8 @@ from ..core.models import TimestampedModel
 
 class Profile(TimestampedModel):
     user = models.OneToOneField("authentication.User", on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-    image = models.URLField(blank=True)
+    bio = models.TextField(blank=True, null=True)
+    image = models.URLField(blank=True, null=True)
     follows = models.ManyToManyField(
         "self", related_name="followed_by", symmetrical=False
     )
