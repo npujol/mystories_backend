e52da9571ddaa5bd946f6230abefcd8f5d1a7d1c
npujol <naivy.luna@gmail.com>
2020-06-08 21:03:28 +0200

    Update drf test base

--- myhistories/apps/core/tests_utils.py
+++ myhistories/apps/core/tests_utils.py
@@ -1,12 +1,20 @@
-# from rest_framework.test import APITestCase, APIClient
-# from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, APIClient
+from ..authentication.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
-# class BaseRestTestCase(APITestCase):
-#     NUMBER_PAGES = 20
+class BaseRestTestCase(APITestCase):
+    NUMBER_PAGES = 20
 
-#     def setUp(self):
-#         super().setUp()
-#         token = Token.objects.get(user__username="lauren")
-#         client = APIClient()
-#         client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+    def setUp(self):
+        super().setUp()
+        self.user = User.objects.create_superuser(
+            username="jonsnow", email="jon@snow.com", password="You_know_nothing123"
+        )
+        refresh = RefreshToken.for_user(self.user)
+        self.tokens = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+        client = APIClient()
+        client.credentials(token=self.tokens["access"])
