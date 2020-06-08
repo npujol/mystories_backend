7dbc86003ea60cf3f985b4395d978c20c023dc54
npujol <naivy.luna@gmail.com>
2020-06-08 21:28:13 +0200

    Update jwt_auth in view

--- myhistories/apps/authentication/views.py
+++ myhistories/apps/authentication/views.py
@@ -3,7 +3,9 @@ from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
 from .renderers import UserJSONRenderer
 from .serializers import LoginSerializer, RegistrationSerializer, UserSerializer
 
@@ -14,12 +16,21 @@ class RegistrationAPIView(APIView):
     renderer_classes = (UserJSONRenderer,)
     serializer_class = RegistrationSerializer
 
+    @swagger_auto_schema(
+        operation_description="Make a new email registration",
+        responses={404: "slug not found"},
+    )
     def post(self, request):
-        user = request.data.get("user", {})
-        serializer = self.serializer_class(data=user)
+        serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        tokens = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+        return Response(tokens, status.HTTP_201_CREATED)
 
 
 class LoginAPIView(APIView):
@@ -27,26 +38,43 @@ class LoginAPIView(APIView):
     renderer_classes = (UserJSONRenderer,)
     serializer_class = LoginSerializer
 
+    @swagger_auto_schema(
+        operation_description="Login, it need username or email, and password ",
+        responses={404: "slug not found"},
+    )
     def post(self, request):
-        user = request.data.get("user", {})
-        serializer = self.serializer_class(data=user)
+        serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        user = User.objects.get(email=request.data.get("email"))
+        refresh = RefreshToken.for_user(user)
+        tokens = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+        return Response(tokens, status=status.HTTP_200_OK)
 
 
 class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    """
+    RetrieveUpdateAPIView description
+
+    retrieve: Retrieve history
+
+    update: Update history
+
+    """
+
     permission_classes = (IsAuthenticated,)
     renderer_classes = (UserJSONRenderer,)
     serializer_class = UserSerializer
 
     def retrieve(self, request, *args, **kwargs):
+        print(request.data.get("user"))
         serializer = self.serializer_class(request.user)
-
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def update(self, request, *args, **kwargs):
-        user_data = request.data.get("user", {})
+        user_data = request.data
 
         serializer_data = {
             "username": user_data.get("username", request.user.username),
