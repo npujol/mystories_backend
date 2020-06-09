6580fe56c03a6dac8c1fac8782d66f6e13a9355d
npujol <naivy.luna@gmail.com>
2020-06-09 18:07:15 +0200

    Update url name to profiles

--- myhistories/apps/profiles/urls.py
+++ myhistories/apps/profiles/urls.py
@@ -5,6 +5,14 @@ from .views import ProfileRetrieveAPIView, ProfileFollowAPIView
 app_name = "profiles"
 
 urlpatterns = [
-    url(r"^profiles/(?P<username>\w+)/?$", ProfileRetrieveAPIView.as_view()),
-    url(r"^profiles/(?P<username>\w+)/follow/?$", ProfileFollowAPIView.as_view()),
+    url(
+        r"^profiles/(?P<username>\w+)/?$",
+        ProfileRetrieveAPIView.as_view(),
+        name="profile_retrieve",
+    ),
+    url(
+        r"^profiles/(?P<username>\w+)/follow/?$",
+        ProfileFollowAPIView.as_view(),
+        name="profile_follow",
+    ),
 ]
