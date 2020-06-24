98faa53e92db1ac6183be703b2d77d2433bf1f4b
npujol <naivy.luna@gmail.com>
2020-06-24 19:05:44 +0200

    Add signal for the notification post speech

--- myhistories/apps/histories/signals.py
+++ myhistories/apps/histories/signals.py
@@ -1,9 +1,12 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.utils.text import slugify
+from django.utils.translation import gettext as _
+from rest_framework.exceptions import NotFound
 
 from ..core.utils import generate_random_string
-from .models import History
+from ..notifications.models import Notification
+from .models import History, Speech
 
 
 @receiver(pre_save, sender=History)
@@ -26,3 +29,20 @@ def add_slug_to_history_if_not_exists(sender, instance, *args, **kwargs):
                 slug = "-".join(parts[:-1])
 
         instance.slug = slug + "-" + unique
+
+
+@receiver(pre_save, sender=Speech)
+def on_change(sender, instance: Speech, **kwargs):
+    if instance.id is None:
+        return
+
+    previous = Speech.objects.get(id=instance.id)
+    if previous.is_ready != instance.is_ready:
+        Notification.objects.create(
+            title=_("The speech is ready"),
+            body=_(
+                "The speech for the history: {} is ready".format(previous.history.slug)
+            ),
+            author=previous.history.author,
+            receiver=previous.history.author,
+        )
